92925a4d2745ccf805906f4db6ae3f0af77e86ad
codeRed26 <99928320+codeRed26@users.noreply.github.com>
2022-03-22 17:58:12 -0700

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,25 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonimg=loadImage("./assets/canon.png");
+    this.cannonBaseimg=loadImage("./assets/cannonBase.png");
+  }
+  display(){
+push()
+//translate(this.x,this.y)
+rotate(this.angle)
+imageMode(CENTER)
+image(this.cannonimg,this.x,this.y,this.width,this.height)
+
+
+pop()
+image(this.cannonBaseimg,70,20,200,200)
+noFill()
+  }
+  
+  
+}
